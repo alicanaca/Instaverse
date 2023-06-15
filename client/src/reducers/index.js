e1af9590228e44ee8b3492194b27e79607979301
Alican Aça <113439488+alicanaca@users.noreply.github.com>
2023-06-15 23:00:07 +0300

    error handling necessary

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,5 +2,5 @@ import { combineReducers } from "redux";
 import stories from './stories'
 
 export default combineReducers({
-    stories: stories
+    stories
 })
--- client/src/reducers/stories.js
+++ client/src/reducers/stories.js
@@ -1,13 +1,7 @@
 
-
 const storyReducer = (state = [], action) => {
-    switch (action.type) {
-        case FETCH_ALL_STORIES:
-            return action.payload;
-        case CREATE_STORY:
-            return state;
-        default:
-            return state;
+    if(action.type === "FETCH_ALL_STORIES"){
+        return action.payload;
     }
 }
 
