00533e5d6c53ea776ad9c8b65c629056bdbad2e0
Alican Aça <113439488+alicanaca@users.noreply.github.com>
2023-06-15 17:26:23 +0300

    app.js updated

--- server/app.js
+++ server/app.js
@@ -3,3 +3,9 @@ import mongoose from 'mongoose'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
+const app = express();
+
+app.use(bodyParser.json({limit: '32mb', extended: 'true'}));
+app.use(bodyParser.urlencoded({limit: '32mb', extended: 'true'}));
+app.use(cors());
+
