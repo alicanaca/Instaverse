d2765515f64904898d4ff4800e3e5f5b66e892ca
Alican Aça <113439488+alicanaca@users.noreply.github.com>
2023-06-15 22:10:33 +0300

    Client-side advanced updates

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import StoryForm from "../StoryForm";
 import StoryList from "../StoryList";
 import { Layout } from "antd";
+import styles from './styles'
+import { useDispatch } from "react-redux";
+import { getStories } from "../../actions/stories"
 
 const { Sider, Content } = Layout;
 
 const Home = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getStories())
+    }, [dispatch])
+
     return(
         <Layout>
-            <Sider width={400}>
+            <Sider style={styles.sider} width={400}>
                 <StoryForm />
             </Sider>
-            <Content>
+            <Content style={styles.content}>
                 <StoryList />
             </Content>
         </Layout>
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from "redux"
+import thunk from "redux-thunk"
+import reducers from "./reducers"
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
 
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 )
